0d0a254a6194aa4ca45a99ddba033b60b6fe81cc
Cristelos <cristelos@msn.com>
2023-05-30 11:43:13 +0200

    first commit

---
+++ src/Form/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Form = () => {
+  return (
+    <>
+        <h2>
+            Fromulario de contacto
+        </h2>
+        <form>
+            
+        </form>
+    </>
+  )
+}
