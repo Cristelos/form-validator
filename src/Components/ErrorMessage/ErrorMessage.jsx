5076b03b0196e91b2fc9eb92f65702bf98d59a3d
Cristelos <cristelos@msn.com>
2023-05-30 13:09:48 +0200

    form and validators are working2

---
+++ src/Components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ErrorMessage = ({ error }) => {
+  return (
+    <div>
+    {error && <>
+        {error.type === "required" && <p>Campo requerido</p>}
+        {error.type === "minLength" && <p>Número de letras insuficiente</p>}
+        {error.type === "min" && <p>Edad no permitida</p>}
+    </>}
+    </div>
+  )
+}
--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '../Components/ErrorMessage/ErrorMessage';
 
 export const Form = () => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+    const onSubmit = (dataF) => {
+        console.log(dataF);
+    }
+
   return (
     <>
         <h2>
             Fromulario de contacto
         </h2>
-        <form>
-            
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type='text' {...register("name", {required: true})}/>
+            <ErrorMessage error={errors.name}/>
+
+            <input type='text' {...register("surname", {required: true, minLength: 5})}/>
+            <ErrorMessage error={errors.surname}/>
+
+            <input type='number' {...register("age", {required: true, min:18})}/>
+            <ErrorMessage error={errors.age}/>
+
+            <input type='email' {...register("email", {required:true})}/>
+            <ErrorMessage error={errors.email}/>
+
+            <button>Enviar </button>
         </form>
     </>
   )
